1ca8e03149227a45191e528d7f74371349e440ce
Torgaeva Kseniya <tkseniya@localhost.localdomain>
2018-04-06 05:46:22 +0300

    Доработки в 5 лабе, убрана часть навозокода

--- Lab3.5/main.c
+++ Lab3.5/main.c
@@ -9,13 +9,13 @@ uint32_t n;
 void initializeArray(int *array, uint32_t);
 
 const uint32_t c = 10;
-//Вариант 12
-//В одномерном массиве, состоящем из n целых элементов, вычислить:
-//1) номер максимального элемента масcива;
-//2) произведение элементов массива, расположенных между первым и вторым нулевыми элементами.
-//Преобразовать  массив  таким  образом,  чтобы  в  первой  его  половине
-//располагались  элементы,  стоявшие  в  нечетных  позициях,  а  во  второй
-//        половине — элементы, стоявшие в четных позициях.
+// Вариант 12
+// В одномерном массиве, состоящем из n целых элементов, вычислить:
+// 1) номер максимального элемента масcива;
+// 2) произведение элементов массива, расположенных между первым и вторым нулевыми элементами.
+// Преобразовать  массив  таким  образом,  чтобы  в  первой  его  половине
+// располагались  элементы,  стоявшие  в  нечетных  позициях,  а  во  второй
+// половине — элементы, стоявшие в четных позициях.
 
 int getMaxArrayNumber(int *arPointer) {
     int maxIndex = 0;
@@ -28,52 +28,48 @@ int getMaxArrayNumber(int *arPointer) {
     return maxIndex;
 }
 
-int* chetNechet(int *pInt) {
+int *chetNechet(int *pInt) {
     int newArrPointer = 0;
-    int array[c];
-    for (int i = 0; i < c; ++i) {
-        if (i % 2) {// нечётные
-            array[newArrPointer] = pInt[i];
-            newArrPointer++;
-        }
+    int *array = calloc(c, sizeof(int));
+    for (int i = 1; i < c; i += 2, newArrPointer++) {
+        array[newArrPointer] = pInt[i];
     }
-    for (int i = 0; i < c; ++i) {
-        if (!(i % 2)) {// чётные
-            array[newArrPointer] = pInt[i];
-            newArrPointer++;
-        }
+    for (int i = 0; i < c; i += 2, newArrPointer++) {
+        array[newArrPointer] = pInt[i];
     }
     return array;
 }
 
 int getMultiplyBetweenZeros(const int *pInt) {
-    int zeroNumber = 0;
-    int firstZeroPointer = -1;
-    for (int i = 0; i < c; ++i) {
-        if (pInt[i] == 0) {
-            zeroNumber++;
-            if (firstZeroPointer == -1) {
-                firstZeroPointer = i;
+    int composition = 1;
+    int firstIndex = -1, secondIndex = -1;
+    int ind = 0;
+    while (ind < c) {
+        if (pInt[ind] == 0) {
+            if (firstIndex < 0) {
+                firstIndex = ind;
+            } else {
+                secondIndex = ind;
+                break;
             }
         }
+        ind++;
     }
 
-    if (zeroNumber != 2) {
-        printf("Неверное число нулей");
-        return 1;
+    if (firstIndex != -1 && secondIndex != -1) {
+        if (secondIndex - firstIndex == 1) {
+            printf("Произведение между двумя нулями это 0");
+            return 0;
+        }
+        for (int i = firstIndex + 1; i < secondIndex; ++i) {
+            composition *= pInt[i];
+        }
+        printf("Произведение между двумя нулями это %i\n", composition);
+        return composition;
+    } else {
+        printf("Нужное количество нулей не найдено");
+        return -1;
     }
-
-    int mult = 1;
-    int lIndex = firstZeroPointer + 1;
-    int fIndex = firstZeroPointer;
-    firstZeroPointer += 1;// сл. элемент
-    while (pInt[firstZeroPointer] != 0) {// обеспечивается безопасность, т.к. два нуля уже гаранитрованно есть
-        mult *= pInt[firstZeroPointer];
-        lIndex = firstZeroPointer;
-        firstZeroPointer++;
-    }
-    printf("Число между двумя нулями это %i\n", (lIndex - fIndex != 1) ? mult : 0);
-    return mult;
 }
 
 
@@ -95,17 +91,20 @@ int main() {
     if (arr == 0) {
         return -1;
     }
-    int array[c];
     // initialization
-    // заполняем случайными числами с 1 до 10
-    initializeArray(array, c);
+    int array[10] = {0, 1, 2, 6, 0, 2, 4, 3, 7, 0};
+    printArray(array, 10);
     initializeArray(arr, n);
 
     getMaxArrayNumber(arr);
     getMultiplyBetweenZeros(array);
 
-    int* arrrrr = chetNechet(array);
+    //arr = {1, 0, 3, 2,};
+    int *arrrrr = chetNechet(array);
     printArray(arrrrr, c);
+
+    free(arrrrr);
+    free(arr);
 
     return 0;
 }
