37bfeff4a098886ed894faeab64324d08d3930ba
Torgaeva Kseniya <tkseniya@192.168.0.4>
2018-04-12 21:20:41 +0300

    либа тестов

---
+++ Lab6/cmake-build-debug/CMakeFiles/Lab6.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tkseniya/Programming/Labs_El/Lab6/main.cpp" "/home/tkseniya/Programming/Labs_El/Lab6/cmake-build-debug/CMakeFiles/Lab6.dir/main.cpp.o"
+  "/home/tkseniya/Programming/Labs_El/Lab6/testMain.cpp" "/home/tkseniya/Programming/Labs_El/Lab6/cmake-build-debug/CMakeFiles/Lab6.dir/testMain.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../mainLab_tests/googletest/include"
+  "../mainLab_tests/googletest"
+  "../lib/googletest/googletest/include"
+  "../lib/googletest/googlemock/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/tkseniya/Programming/Labs_El/Lab6/cmake-build-debug/lib/googletest/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab6/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/tkseniya/Programming/Labs_El/Lab6/lib/googletest/googlemock
+# Build directory: /home/tkseniya/Programming/Labs_El/Lab6/cmake-build-debug/lib/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
